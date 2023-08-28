0480cfbf66d89f17c3c422e10f5856ed9c83f18c
Dmytro Maslakov <dmytro.maslakov03@gmail.com>
2023-08-28 09:52:30 +0300

    validation lesson

--- api/src/Controller/TestController.php
+++ api/src/Controller/TestController.php
@@ -10,12 +10,15 @@ use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class TestController extends AbstractController
 {
@@ -26,42 +29,52 @@ class TestController extends AbstractController
     private EntityManagerInterface $entityManager;
 
     /**
-     * @param EntityManagerInterface $entityManager
+     * @var DenormalizerInterface
      */
-    public function __construct(EntityManagerInterface $entityManager)
+    private DenormalizerInterface $denormalizer;
+
+    /**
+     * @var ValidatorInterface
+     */
+    private ValidatorInterface $validator;
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     * @param DenormalizerInterface $denormalizer
+     * @param ValidatorInterface $validator
+     */
+    public function __construct(EntityManagerInterface $entityManager,
+                                DenormalizerInterface  $denormalizer,
+                                ValidatorInterface     $validator)
     {
         $this->entityManager = $entityManager;
+        $this->denormalizer = $denormalizer;
+        $this->validator = $validator;
     }
 
     /**
      * @return JsonResponse
      */
     #[Route(path: 'test', name: 'app_test')]
-    public function test(): JsonResponse
+    public function test(Request $request): JsonResponse
     {
-        $user = $this->getUser();
+        $requestData = json_decode($request->getContent(), true);
 
-        $products = $this->entityManager->getRepository(Product::class)->findAll();
+        $product = $this->denormalizer->denormalize($requestData, Product::class, "array");
 
-        if (in_array(User::ROLE_ADMIN, $user->getRoles())) {
-            return new JsonResponse($products);
+        $errors = $this->validator->validate($product);
+
+        if (count($errors) > 0) {
+            return new JsonResponse((string)$errors);
         }
-        return new JsonResponse($this->fetchedProductsForUser($products));
+
+        /*        $products = $this->entityManager->getRepository(Product::class)->findAll();
+
+                if (in_array(User::ROLE_ADMIN, $user->getRoles())) {
+                    return new JsonResponse($products);
+                }*/
+
+        return new JsonResponse();
     }
 
-    /**
-     * @param array $products
-     * @return array
-     */
-    public function fetchedProductsForUser(array $products): array{
-        $fetchedProductsForUser = null;
-        /** @var Product $product */
-        foreach ($products as $product) {
-            $tmpProductData = $product->jsonSerialize();
-
-            unset($tmpProductData['description']);
-            $fetchedProductsForUser[] = $tmpProductData;
-        }
-        return $fetchedProductsForUser;
-    }
 }
---
+++ api/src/Validator/Constraints/ProductConstraint.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Validator\Constraints;
+
+
+use Attribute;
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ */
+#[Attribute] class ProductConstraint extends Constraint
+{
+    /**
+     * @return string
+     */
+    public function validatedBy(): string
+    {
+        return get_class($this) . "Validator";
+    }
+
+    /**
+     * @return array|string|string[]
+     */
+    public function getTargets(): array|string
+    {
+        return self::CLASS_CONSTRAINT;
+    }
+}
